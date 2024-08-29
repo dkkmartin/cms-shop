9202fe8dc53ff0563741a9d744af34a83cdb1f1a
Martin Bruun <martinbruun1995@gmail.com>
2024-08-29 11:32:53 +0200

    chore: Update utils.ts with IP constant

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export const IP = '192.168.0.9';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
