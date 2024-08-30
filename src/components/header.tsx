7bf7d87d8d8840936ebbd672d0f9ac48aa5e739c
Martin Bruun <martinbruun1995@gmail.com>
2024-08-30 23:19:14 +0200

    feat: Add Footer component for displaying the website footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -27,7 +27,7 @@ export default function HeaderNav() {
   );
 
   return (
-    <header className="border-b">
+    <header className="border-b max-h-16">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex gap-4">
