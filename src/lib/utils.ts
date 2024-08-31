a985ed28df3d5432bea7bf2e19f4adf71b4c5162
Martin Bruun <martinbruun1995@gmail.com>
2024-08-31 13:33:59 +0200

    refactor: Add capitalizeAndRemoveHyphen function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,10 @@ export const IP = '192.168.0.9';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function capitalizeAndRemoveHyphen(input: string): string {
+  return input
+    .split('-')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
